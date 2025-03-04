99872087c47b6a5961d91d418c138dd5da3821ed
prem-thatikonda <142595376+prem-thatikonda@users.noreply.github.com>
2025-03-04 21:26:26 +0530

    added loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -3,37 +3,43 @@ import "./App.css";
 
 function App() {
   const images = [
-    {
-      photo: "/man.gif",
-      interval: 2000,
-    },
-    {
-      photo: "/photo.gif",
-      interval: 1000,
-    },
-    {
-      photo: "/mom_beat.jpg",
-    },
+    { photo: "/man.gif", interval: 2000 },
+    { photo: "/photo.gif", interval: 1000 },
+    { photo: "/mom_beat.jpg" },
   ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    if (currentIndex === images.length - 1) return; // Stop at last image
+    const loadingTimeout = setTimeout(() => setLoading(false), 3000);
+
+    return () => clearTimeout(loadingTimeout);
+  }, []);
+
+  useEffect(() => {
+    if (loading || currentIndex === images.length - 1) return; // Stop if loading or last image
 
     const interval = setInterval(() => {
       setCurrentIndex((prevIndex) => prevIndex + 1);
-    }, images[currentIndex]?.interval || 2000); // Use specified interval or default 2000ms
+    }, images[currentIndex]?.interval || 2000);
 
     return () => clearInterval(interval);
-  }, [currentIndex]);
+  }, [currentIndex, loading]);
 
   return (
     <section className="main">
-      <div className="photo">
-        <img src={images[currentIndex].photo} alt="cycling images" />
-        {currentIndex === 2 && <h1>I'll show this to ya mum nigga</h1>}
-      </div>
+      {loading ? (
+        <div className="loading-screen">
+          <img src="/logo.png" alt="Logo" className="loading-logo" />
+          <img src="/name.png" alt="logo" className="loading-name" />
+        </div>
+      ) : (
+        <div className="photo">
+          <img src={images[currentIndex].photo} alt="cycling images" />
+          {currentIndex === 2 && <h1>I'll show this to ya mum nigga</h1>}
+        </div>
+      )}
     </section>
   );
 }
